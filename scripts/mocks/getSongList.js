7166ba5c544b6978c05f8d75bfda58e010259393
John McGorey <j.t.mcgorey@gmail.com>
2021-06-14 21:52:40 -0500

    Added getSongList mock

---
+++ scripts/mocks/getSongList.js
@@ -0,0 +1,23 @@
+export async function getSongList() {
+	return [
+		{ id: 1, title: `Don't Be Cruel`, artist: "Elvis Presley", year: 1965 },
+		{ id: 2, title: `Love Me Tender`, artist: "Elvis Presley", year: 1965 },
+		{ id: 3, title: `I'm All Shook Up`, artist: "Elvis Presley", year: 1965 },
+		{ id: 4, title: `Teddy Bear`, artist: "Elvis Presley", year: 1965 },
+		{ id: 5, title: `Tutti Frutti`, artist: "Little Richard", year: 1965 },
+		{ id: 6, title: `Long Tall Sally`, artist: "Little Richard", year: 1965 },
+		{ id: 7, title: `Slippin' and Slidin'`, artist: "Little Richard", year: 1965 },
+		{ id: 8, title: `Good Golly Miss Molly`, artist: "Little Richard", year: 1965 },
+		{ id: 9, title: `Roll Over Beethoven`, artist: "Chuck Berry", year: 1965 },
+		{ id: 10, title: `Sweet Little Sixteen`, artist: "Chuck Berry", year: 1965 },
+		{ id: 11, title: `School Days`, artist: "Chuck Berry", year: 1965 },
+		{ id: 12, title: `Johnny B. Goode`, artist: "Chuck Berry", year: 1965 },
+		{ id: 13, title: `Rock 'n Roll Music`, artist: "Chuck Berry", year: 1965 },
+		{ id: 14, title: `Maybelline`, artist: "Chuck Berry", year: 1965 },
+		{ id: 15, title: `Take It Easy`, artist: "Eagles", year: 1972 },
+		{ id: 16, title: `Hotel California`, artist: "Eagles", year: 1976 },
+		{ id: 17, title: `Take It to the Limit`, artist: "Eagles", year: 1975 },
+		{ id: 18, title: `Lyin' Eyes`, artist: "Eagles", year: 1975 },
+		{ id: 19, title: `Desperado`, artist: "Eagles", year: 1973 },
+	];
+}
