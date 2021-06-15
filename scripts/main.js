39b5e3946121186c318c82ad0b9d771a939108b9
John McGorey <j.t.mcgorey@gmail.com>
2021-06-14 23:16:48 -0500

    Added queue view in index.html

--- scripts/main.js
+++ scripts/main.js
@@ -3,36 +3,51 @@ import { addSongToQueue } from "./requests/addSongToQueue.js";
 import { getQueue } from "./requests/mocks/data.js";
 
 async function createSongList() {
+	// Get the list of songs
 	const songs = await getSongList();
 
+	// Create a list item for each song in the list
 	let songsHtml = "";
 	songs.forEach((s) => {
 		songsHtml += `<li>
-                (${s.id}) ${s.title} - ${s.title} (${s.year})
+                (${s.id}) ${s.title} - ${s.artist} (${s.year})
                 <button class="songAddButton" data-song-id=${s.id} type="button">+</button>
             </li>`;
 	});
 	const songListElement = document.getElementById("songList");
 	songListElement.innerHTML = songsHtml;
 
+	// Add click handlers to all of the "add song" buttons
 	const buttons = document.getElementsByClassName("songAddButton");
 	const numButtons = buttons.length;
 	for (let i = 0; i < numButtons; i++) {
-		buttons[i].onclick = addSong;
+		buttons[i].onclick = function (e) {
+			const button = e.target;
+			const songId = button.dataset.songId;
+			addSong(songId);
+		};
 	}
 }
 
-function addSong(e) {
-	const button = e.target;
-	const songId = button.dataset.songId;
-	console.log(songId);
-
-	addSongToQueue(songId);
+async function addSong(songId) {
+	await addSongToQueue(songId);
+	await updateSongQueue();
 }
 
-createSongList();
+async function updateSongQueue() {
+	const queue = getQueue();
 
-const queueButton = document.getElementById("queueLogBtn");
-queueButton.onclick = function (e) {
-	console.log("Song Queue:", getQueue());
-};
+	// Create a list item for each song in the queue
+	let songsHtml = "";
+	for (const s of queue) {
+		songsHtml += `<li>(${s.id}) ${s.title} - ${s.artist} (${s.year})</li>`;
+	}
+
+	if (!songsHtml) songsHtml = "None";
+	const queueList = document.getElementById("songQueue");
+	queueList.innerHTML = songsHtml;
+}
+
+// Main
+createSongList();
+updateSongQueue();
